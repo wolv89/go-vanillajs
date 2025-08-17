c0f786ea91fc6d20e240ba297bcc742d7adfeead
Peter <peter.delafosse@gmail.com>
2025-08-17 14:46:56 +1000

    Basic implementation of custom logger

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"frontendmasters.com/reelingit/logger"
 )
 
+func initialiseLogger() *logger.Logger {
+
+	l, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("unable to set up logger: %v", err)
+	}
+	return l
+
+}
+
 func main() {
+
+	// Server Logger
+	slog := initialiseLogger()
+	defer slog.Close()
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8017"
+
+	fmt.Println("starting server with address:", addr)
 	err := http.ListenAndServe(addr, nil)
 
 	if err != nil {
-		log.Fatalf("Server failed: %v", err)
+		slog.Error("server failed", err)
+		log.Fatalf("server failed: %v", err)
 	}
 
 }
